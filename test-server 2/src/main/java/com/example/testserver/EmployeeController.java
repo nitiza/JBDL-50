f6d1bd11edd9517f262ef89e58762d9a7e45cde1
Nitisha Agarwal <nitishaagarwal3@gmail.com>
2023-07-16 17:41:03 +0530

    Test rest api

--- test-server 2/src/main/java/com/example/testserver/EmployeeController.java
+++ test-server 2/src/main/java/com/example/testserver/EmployeeController.java
@@ -1,48 +1,70 @@
 package com.example.testserver;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EmployeeController {
 
-    private static Logger logger = LoggerFactory.getLogger(EmployeeController.class);
-
     /**
-     * Take an input(name) from the FE and return "Hello " + person name
+     * CRUD operations that can handle employee lifecycle
      */
 
-    /**
-     * Base url : Domain name / IP + Port
-     * Steps to write an API
-     * Define the HTTP method
-     * Define endpoint / API Path
-     * Define how to take input if reqd [Optional]
-     * Define teh response type
-     */
+    private HashMap<Integer, Employee> employeeHashMap = new HashMap<>();
 
-    // /hello?name=Nitisha
-
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam("name") String n) {
-
-        return "Hello " + n + "!!";
-
+    @GetMapping("/employee")
+    public List<Employee> getEmployees() {
+        // return the list of all the employees
+        return this.employeeHashMap.values().stream().collect(Collectors.toList());
     }
 
-    @GetMapping("/logtest")
-    public String helloWorld(){
+    @GetMapping("/employee/id/{id}")
+    public Employee getEmployeeById(@PathVariable("id") int id) {
+        return this.employeeHashMap.get(id);
+    }
 
-        logger.error("This is an error log");
-        logger.warn("This is a warn log");
-        logger.info("This is a info log");
-        logger.debug("This is a debug log");
-        logger.trace("This is a trace log");
+    @PostMapping("/employee")
+    public void createEmployee(@RequestBody Employee employee) throws Exception {
+        if (employee.getId() == null) {
+            throw new Exception("Employee id cannot be null");
+        }
+        this.employeeHashMap.put(employee.getId(), employee);
+    }
 
-        return "Hello World";
+    @PutMapping("/employee/id/{id}")
+    public Employee updateEmployee(@RequestBody Employee employee) throws Exception {
 
+        if (!this.employeeHashMap.containsKey(employee.getId()) || employee.getId() == null) {
+            throw new Exception("Not a valid employee, updation failed");
+        }
+
+        return this.employeeHashMap.put(employee.getId(), employee);
+    }
+
+    @PatchMapping("/employee/id/{id}")
+    public Employee updatePartialEmployee(@RequestBody Employee employee) throws Exception {
+
+        if (employee.getId() == null || !this.employeeHashMap.containsKey(employee.getId())) {
+            throw new Exception("Not a valid employee, updation failed");
+        }
+
+        Employee employeeFromMap = this.employeeHashMap.get(employee.getId());
+
+        if (employee.getAge() != null) {
+            employeeFromMap.setAge(employee.getAge());
+        }
+        if (employee.getName() != null) {
+            employeeFromMap.setName(employee.getName());
+        }
+
+        return this.employeeHashMap.put(employee.getId(), employeeFromMap);
+    }
+
+    @DeleteMapping("/employee")
+    public Employee deleteEmployee(@RequestParam("id") int id) {
+        return this.employeeHashMap.remove(id);
     }
 }
---
+++ test-server 2/src/main/java/com/example/testserver/ImageController.java
@@ -0,0 +1,30 @@
+package com.example.testserver;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.awt.*;
+
+@RestController
+public class ImageController {
+
+    /**
+     * input: an integer id
+     * output: an image corresponding to the id from lorem picsum website
+     */
+
+    @GetMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getImage(@RequestParam(value = "id") int id,
+                           @RequestParam(value = "width", required = false, defaultValue = "200") int width,
+                           @RequestParam(value = "height", required = false, defaultValue = "300") int height) {
+
+        String url = "https://picsum.photos/id/" + id + "/" + width + "/" + height;
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, byte[].class);
+    }
+}
